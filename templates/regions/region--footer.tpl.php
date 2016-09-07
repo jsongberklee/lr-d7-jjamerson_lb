ed4c806c8643b8ef610ba561522df8ec31d9d402
jsongberklee <jsong@berklee.edu>
2016-09-07 10:08:46 -0400

    footer link update 2

--- templates/regions/region--footer.tpl.php
+++ templates/regions/region--footer.tpl.php
@@ -8,7 +8,7 @@
           <ul class="bar">
 				    <li><a href="/about-us/services/interlibrary-loan-request-form">Interlibrary Loan Request</a></li>
 				    <li><a href="http://ask.library.berklee.edu/widget_standalone.php?la_widget_id=3283">Ask A Librarian</a></li>
-				    <li><a href="/about-us/frequently-asked-questions">FAQ</a></li>
+				    <li><a href="http://ask.library.berklee.edu/">FAQ</a></li>
 				    <li><a href="http://www.berklee.edu/">Berklee.edu</a></li>
 				    <li><a href="/about-us/contact-us">Contact Us</a></li>
 					<li><a href="http://learningcenter.berklee.edu/">Learning Center</a></li>
